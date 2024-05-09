fdf66db411495a37cc58a016ee570970a984698d
ahmadansari <81446944+ahmad007125@users.noreply.github.com>
2024-05-10 01:39:06 +0500

    Fix Blog things

--- blog.php
+++ blog.php
@@ -136,7 +136,8 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
        </aside>
   </section>
     </div>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
   
    <script>
    	 $(document).ready(function(){
